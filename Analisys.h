ba4628391ed07d780080fb0d90f4a20a001c59cf
ndrg <ndrgrimm@gmail.com>
2018-03-10 23:36:24 +0100

    Change TH1->TNtupla for flexibility. Filter still not working
    I changed TH1-> TNtupla to have all the info on peak for offline
    data analysis
    PeakInfo contain:
    "Amp:Pos:Numbers:index"
    where
     Amp:Amplitude
     Pos:Time Position
     Numbers: Numbers of peak for frame
     index: index of peak find in the frame
    
    Exmple:
     500:10:3:0
     430:15:3:1
     300:40:3:2
     520:12:2:0
     200:30:2:1
    
    Filter is still not working

--- Analisys.h
+++ Analisys.h
@@ -1,9 +1,12 @@
 #include"tsegment.h"
 #include<root/TSpectrum.h>
 #include<root/TH1F.h>
+#include<root/TH1S.h>
+#include<root/TNtuple.h>
+
 #include<iostream>
 
-void Analysis ( TH1S* PeakNumber,TH1S* PeakAmplitude,TH1F* SpectrumFiltred,TH1F* Spectrum,TSegment * inData,uint NumberPeak, Double_t ThreadShold, unsigned int Start, int unsigned Stop=0 )
+void Analysis ( TNtuple* PeakInfo,TH1F* SpectrumFiltred,TH1F* Spectrum,TSegment * inData,uint NumberPeakMax, Double_t ThreadShold, unsigned int Start, int unsigned Stop=0 )
 {
 
 
@@ -18,40 +21,67 @@ void Analysis ( TH1S* PeakNumber,TH1S* PeakAmplitude,TH1F* SpectrumFiltred,TH1F*
     }
 
 
+    TH1F * TmpSpectrum= ( TH1F* ) Spectrum->Clone();
+    TSpectrum* Analyzer;
 
+
+    TmpSpectrum->SetName ( "tmp" );
     double*   ptrArray_f=new double[lenght];
+    bool SomethingBigger=false;
     for ( int i=0; i <lenght; ++i ) {
         ptrArray_f[i]=-1*inData->GetSample ( Start+i ); // change sign of data
         Spectrum->Fill ( i,ptrArray_f[i] );
+        if ( ptrArray_f[i] < ThreadShold ) {
+            TmpSpectrum->SetBinContent ( i,0 );
+        } else {
+            TmpSpectrum->SetBinContent ( i,ptrArray_f[i] );
+            SomethingBigger=true;
+        }
+    }
+    if ( !SomethingBigger ) {
+        //if(false){
+        PeakInfo->Fill ( 0, 0, -1 );
+        return;
+    } else {
+        Analyzer=new TSpectrum();
 
     }
-    TSpectrum* Analyzer=new TSpectrum();
 
-    double *destVec=new double[lenght];
-    Analyzer->SearchHighRes ( ptrArray_f,destVec,lenght,5, ThreadShold,kFALSE,3,kFALSE,1 );
-    //std::cout << "Numero picchi: " << Analyzer->GetNPeaks() << std::endl;
-    PeakNumber->Fill ( Analyzer->GetNPeaks() );
 
-    for ( int i=0; i<Analyzer->GetNPeaks(); ++i ) {
-        PeakAmplitude->Fill ( Analyzer->GetPositionY() [i] );
+
+    std::cout << "Max: " << TmpSpectrum->GetBinContent ( TmpSpectrum->GetMaximumBin() ) << std::endl;
+
+
+    Analyzer->Search ( TmpSpectrum, 2, "", 0.05 );
+
+    Double_t * dest   = new Double_t[lenght];
+    //Analyzer->SearchHighRes ( ptrArray_f,dest,lenght,2, 10  ,kFALSE,3,kFALSE,1 );
+
+    std::cout << "PeakNumber: " << Analyzer->GetNPeaks() << std::endl;
+
+    for ( int peak=0; peak<Analyzer->GetNPeaks(); ++peak ) {
+        PeakInfo->Fill ( ptrArray_f[int(Analyzer->GetPositionX() [peak]+0.5)],  //TEST
+                         Analyzer->GetPositionX() [peak]+0.5,
+                         Analyzer->GetNPeaks(),
+                         peak ); 
     }
 
-    if ( Analyzer->GetNPeaks() == NumberPeak ) {
-        for ( int peak =0; peak < NumberPeak && peak < Analyzer->GetNPeaks() ; ++peak ) {
-            SpectrumFiltred->Fill ( Analyzer->GetPositionX() [peak] ); // TODO signal really low
+
+
+    if ( Analyzer->GetNPeaks() <= NumberPeakMax ) {
+        for ( int peak =0; peak < NumberPeakMax && peak < Analyzer->GetNPeaks() ; ++peak ) {
+            SpectrumFiltred->Fill ( int(Analyzer->GetPositionX() [peak]) );          
         }
     }
 
-
-
     delete Analyzer;
     delete[] ptrArray_f;
-    delete[] destVec;
+    delete[] dest;
 
 }
 
 
-void AnalysisOld( TH1F* SpectrumFiltred,TH1F* Spectrum,TSegment * inData, Double_t ThreadShold, unsigned int Start, int unsigned Stop=0 )
+void AnalysisOld ( TH1F* SpectrumFiltred,TH1F* Spectrum,TSegment * inData, Double_t ThreadShold, unsigned int Start, int unsigned Stop=0 )
 {
 
 
@@ -65,18 +95,18 @@ void AnalysisOld( TH1F* SpectrumFiltred,TH1F* Spectrum,TSegment * inData, Double
         return;
     }
 
-    if( Spectrum != NULL ){
-        
-    for ( int i=0; i <lenght; ++i ) {
-            Spectrum->Fill ( Start+i,-1.* (inData->GetSample( Start+i ) ) );
+    if ( Spectrum != NULL ) {
+
+        for ( int i=0; i <lenght; ++i ) {
+            Spectrum->Fill ( Start+i,-1.* ( inData->GetSample ( Start+i ) ) );
 
         }
-        
+
     }
 
     for ( int i=0; i <lenght; ++i ) {
 
-        if ( -1.* inData->GetSample( Start+i )  > ThreadShold ) {
+        if ( -1.* inData->GetSample ( Start+i )  > ThreadShold ) {
             SpectrumFiltred->Fill ( i,+1 );
         } else {
             SpectrumFiltred->Fill ( i,+0 );
@@ -86,3 +116,82 @@ void AnalysisOld( TH1F* SpectrumFiltred,TH1F* Spectrum,TSegment * inData, Double
     }
 
 }
+
+void AnalysisIntegralDiscriminator ( TH1F* SpectrumFiltred,TH1F* Spectrum,TSegment * inData, Double_t ThreadShold, unsigned int Start, int unsigned Stop=0 )
+{
+
+
+
+    if ( Stop==0 ) {
+        Stop=inData->GetSampleCount();
+    }
+    unsigned int lenght= Stop-Start;
+    // std::cout << lenght << std::endl;
+    if ( lenght > inData->GetSampleCount() || Stop > inData->getArrSize() ) {
+        return;
+    }
+
+    if ( Spectrum != NULL ) {
+
+        for ( int i=0; i <lenght; ++i ) {
+            Spectrum->Fill ( Start+i,-1.* ( inData->GetSample ( Start+i ) ) );
+
+        }
+
+    }
+
+    for ( int i=0; i <lenght; ++i ) {
+
+        if ( -1.* inData->GetSample ( Start+i )  > ThreadShold ) {
+            if ( -1.* inData->GetSample ( Start+i-1 )  <= ThreadShold ) {
+                SpectrumFiltred->Fill ( i,+1 );
+            }
+
+        } else {
+            SpectrumFiltred->Fill ( i,+0 ); //FIXME Questa linea serva?
+        }
+
+
+    }
+
+}
+
+
+// void CFD( TH1F* SpectrumFiltred,TH1F* Spectrum,TSegment * inData, Double_t ThreadShold, unsigned int Start, int unsigned Stop=0 )
+// {
+//
+//
+//
+//     if ( Stop==0 ) {
+//         Stop=inData->GetSampleCount();
+//     }
+//     unsigned int lenght= Stop-Start;
+//     // std::cout << lenght << std::endl;
+//     if ( lenght > inData->GetSampleCount() || Stop > inData->getArrSize() ) {
+//         return;
+//     }
+//
+//     if( Spectrum != NULL ){
+//
+//     for ( int i=0; i <lenght; ++i ) {
+//             Spectrum->Fill ( Start+i,-1.* (inData->GetSample( Start+i ) ) );
+//
+//         }
+//
+//     }
+//
+//     for ( int i=0; i <lenght; ++i ) {
+//
+//         if ( -1.* inData->GetSample( Start+i )  > ThreadShold ) {
+//             if ( -1.* inData->GetSample( Start+i-1 )  < -1.* inData->GetSample( Start+i ) ){
+//                 SpectrumFiltred->Fill ( i,+1 );
+//             }
+//
+//         } else {
+//             SpectrumFiltred->Fill ( i,+0 ); //FIXME Questa linea serva?
+//         }
+//
+//
+//     }
+//
+// }
